378dd03dba1f43fc825226409fadd68895ff752e
Matteo Vaccari <vaccari@pobox.com>
2017-01-11 10:35:34 +0100

    now the smoke test fails for the right reason

--- script/smoke-test.rb
+++ script/smoke-test.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 
 require 'capybara'
-session = Capybara::Session.new(:selenium)
+require 'minitest/autorun'
 
-session.visit "http://test.xpenses.it.s3-website.eu-central-1.amazonaws.com/"
+class SmokeTest < Minitest::Test
 
-if session.has_content?("Expenses summary")
-  puts "All shiny, captain!"
-else
-  puts ":( no tagline fonud, possibly something's broken"
-  exit(-1)
+  def setup
+    system 'script/deploy.sh'
+  end
+
+  def test_summary_row
+    session = Capybara::Session.new(:selenium)
+    session.visit "http://test.xpenses.it.s3-website.eu-central-1.amazonaws.com/"
+    p session.text
+    assert session.has_content?("Expenses summary"), "not the right page?"
+    table = session.find('#summary-table tbody')
+
+    table.find('td:nth-of-type(1)').assert_text('2016-09')
+    table.find('td:nth-of-type(2)').assert_text('750.53')
+  end
 end
-
