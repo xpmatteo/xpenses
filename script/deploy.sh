93e8b490cf8a3be483ec69a3989e74152f85125c
Matteo Vaccari <vaccari@pobox.com>
2017-01-11 12:17:39 +0100

    smoke test fails because of missing upload script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,23 @@
 set -e
 cd "$(dirname $0)/.."
 
-bucket='test.xpenses.it'
+env='test'
+
+bucket="$env.xpenses.it"
 
 aws s3api put-bucket-policy --bucket $bucket --policy file://config/hosting-bucket-policy.json
+
+aws dynamodb create-table \
+  --table-name $env.movements \
+  --attribute-definitions \
+    AttributeName=id,AttributeType=S \
+    AttributeName=date,AttributeType=S \
+  --key-schema \
+    AttributeName=id,KeyType=HASH \
+    AttributeName=date,KeyType=RANGE \
+  --provisioned-throughput \
+    ReadCapacityUnits=5,WriteCapacityUnits=5 \
+  > /dev/null
+
 aws s3 cp web/index.html s3://$bucket/
+
--- script/smoke-test.rb
+++ script/smoke-test.rb
@@ -6,8 +6,14 @@ require 'minitest/autorun'
 class SmokeTest < Minitest::Test
 
   def setup
-    system 'script/deploy.sh'
+    $env = 'test'
+    sh "aws dynamodb delete-table --table-name #$env.movements > /dev/null"
+    sh 'script/deploy.sh'
+    sh 'script/upload test-data/isp-movements-short.xls'
   end
+
+  # See here for Capybara docs
+  # http://www.rubydoc.info/github/jnicklas/capybara/master/Capybara/Session
 
   def test_summary_row
     session = Capybara::Session.new(:selenium)
@@ -18,5 +24,11 @@ class SmokeTest < Minitest::Test
 
     table.find('td:nth-of-type(1)').assert_text('2016-09')
     table.find('td:nth-of-type(2)').assert_text('750.53')
+  end
+
+  private
+
+  def sh command
+    assert system(command), "Failed execution of '#{command}'"
   end
 end
