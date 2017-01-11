b51f2b0f12502bddded9d5d0a1ee51b6c5041bcd
Matteo Vaccari <vaccari@pobox.com>
2017-01-11 17:15:45 +0100

    now the app is served solely by sinatra

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 require 'json'
 
+get '/' do
+  send_file 'public/index.html', type: :html
+end
+
 get '/api/summary' do
   content_type :json
-  { month: '2016-09', total: '750.53'}.to_json
+  [{ month: '2016-09', total: '750.53'}].to_json
 end
+
