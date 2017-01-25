96e741797c640c271a33f4b9a7f1248093593260
Matteo Vaccari <mvaccari@thoughtworks.com>
2017-01-25 10:29:11 +0100

    we now have provisioned the dynamodb table as well

--- script/provision.sh
+++ script/provision.sh
@@ -18,19 +18,4 @@ terraform remote config \
     -backend-config="key=network/terraform.tfstate" \
     -backend-config="region=eu-central-1"
 
-#
-#
-# if ! aws dynamodb list-tables | grep -q $env.movements; then
-#   echo "Creating $env.movements"
-#   aws dynamodb create-table \
-#     --table-name $env.movements \
-#     --attribute-definitions \
-#       AttributeName=id,AttributeType=S \
-#       AttributeName=date,AttributeType=S \
-#     --key-schema \
-#       AttributeName=id,KeyType=HASH \
-#       AttributeName=date,KeyType=RANGE \
-#     --provisioned-throughput \
-#       ReadCapacityUnits=5,WriteCapacityUnits=5 \
-#     > /dev/null
-# fi
+terraform apply provision
--- script/terraform-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-terraform remote config -backend=s3 \
-  -backend-config="bucket=mv-free-terraform-experiment-state" \
-  -backend-config="key=network/terraform.tfstate" \
-  -backend-config="region=eu-central-1"
