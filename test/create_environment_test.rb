45eb4bb616c022797432f3aa1ef779f7fe23abdf
Matteo Vaccari <mvaccari@thoughtworks.com>
2017-01-25 17:34:24 +0100

    first test in creating instance almost runs

--- test/create_environment_test.rb
+++ test/create_environment_test.rb
@@ -3,18 +3,30 @@ require 'minitest/autorun'
 require 'aws-sdk'
 require 'ostruct'
 
+# see docs here: http://docs.aws.amazon.com/sdkforruby/api/Aws/EC2/Instance.html
+
 $region = 'eu-central-1'
+$key_name = 'matteo-free'
 
 def create_instance name, env, params
   ec2 = Aws::EC2::Resource.new(region: $region)
-  ec2.create_instances(params)
+  defaults = {
+    min_count: 1,
+    max_count: 1,
+  }
+
+  instance = ec2.create_instances(defaults.merge(params))
+  instance.first.wait_until_running
+  instance.first.create_tags({ tags: [
+    { key: 'Name', value: name },
+    { key: 'Env', value: env },
+  ]})
+  return instance.first
 end
 
 def find_instance name, env
   ec2 = Aws::EC2::Resource.new(region: $region)
 
-  # Get all instances with tag key 'Group'
-  # and tag value 'MyGroovyGroup':
   instances= ec2.instances({filters: [
     {name: 'tag:Name', values: [name]},
     {name: 'tag:Env', values: [env]},
@@ -26,24 +38,46 @@ def find_instance name, env
   end
 end
 
-class CreateEnvironmentTest < Minitest::Test
-  def test_create_instance
-    create_instance "a", "b", {}
+def delete_instances env
+  ec2 = Aws::EC2::Resource.new(region: $region)
 
-    create_instance "xpenses_host", "test", {
+  instances= ec2.instances({filters: [
+    {name: 'tag:Env', values: [env]},
+  ]})
+  instances.each do |i|
+    if i.exists?
+      case i.state.code
+      when 48  # terminated
+        puts "#{i} is already terminated"
+      else
+        i.terminate
+      end
+    end
+  end
+end
+
+class CreateEnvironmentTest < Minitest::Test
+
+  def setup
+    @instance = create_instance "xpenses_host", "test", {
       image_id: "ami-211ada4e",
-      associate_public_ip_address: true,
-      key_name: "${var.key_name}",
+      key_name: $key_name,
       instance_type: "t2.micro"
     }
-#      i.vpc_security_group_ids = ["${aws_security_group.xpenses_host_sg.id}"]
-
-    found_instance = find_instance "xpenses_host", "test"
-    assert_equal "ami-211ada4e", found_instance.ami
-    assert_match /35\.\d{1-3}\.\d{1-3}\.\d{1-3}/, found_instance.public_ip
   end
 
-  def test_instance_already_existing
+  def teardown
+    delete_instances "test"
+  end
+
+  def test_create_instance
+    found_instance = find_instance "xpenses_host", "test"
+    assert_equal "ami-211ada4e", found_instance.image_id
+    assert_match /abc/, found_instance.public_ip_address
+    assert_match /35\.\d{1-3}\.\d{1-3}\.\d{1-3}/, found_instance.public_ip_address
+  end
+
+  def xtest_instance_already_existing
 
   end
 end
