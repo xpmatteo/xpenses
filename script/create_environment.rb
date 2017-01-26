25d5c69e82fe776dc393583064c4a3c457ddd2e3
Matteo Vaccari <mvaccari@thoughtworks.com>
2017-01-26 12:36:00 +0100

    I can now create an env with code!

--- script/create_environment.rb
+++ script/create_environment.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+$:.push File.dirname(__FILE__) + '/../lib/'
+
 if ARGV.size != 1
   puts "Usage: #{$0} <environment>"
   exit 1
@@ -10,6 +12,7 @@ include Infrastructure
 
 @env = ARGV[0]
 
+puts "Creating instance..."
 sg = create_security_group 'xpenses-host', @env do |sg|
   sg.authorize_ingress({
     ip_permissions: [
@@ -42,12 +45,32 @@ end
   security_group_ids: [sg.id],
 }
 
-# dynamodb_client = Aws::DynamoDB::Client.new(region: $region)
-# request = {
-#   attribute_definitions:    attribute_defs,
-#   table_name:               @name,
-#   key_schema:               key_schema,
-#   provisioned_throughput:   { read_capacity_units: 5, write_capacity_units: 10 }
-# }
-# dynamodb_client.create_table(request)
-# dynamodb_client.wait_until(:table_exists, table_name: @name)
+
+
+table_name = "xpenses_movements_#{@env}"
+puts "Creating table #{table_name}"
+dyn = Aws::DynamoDB::Resource.new(region: $region)
+if dyn.tables.find { |t| t.table_name == table_name }
+  puts "Table #{table_name} already exists"
+else
+  attribute_defs = [
+    { attribute_name: 'id',        attribute_type: 'S' },
+    { attribute_name: 'date',      attribute_type: 'S' },
+  ]
+
+  key_schema = [
+    { attribute_name: 'id', key_type: 'HASH' },
+    { attribute_name: 'date', key_type: 'RANGE' },
+  ]
+
+  request = {
+    attribute_definitions:    attribute_defs,
+    table_name:               table_name,
+    key_schema:               key_schema,
+    provisioned_throughput:   { read_capacity_units: 5, write_capacity_units: 5 }
+  }
+
+  dynamodb_client = Aws::DynamoDB::Client.new(region: $region)
+  dynamodb_client.create_table(request)
+  dynamodb_client.wait_until(:table_exists, table_name: table_name)
+end
--- script/create_environment.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-
-if ARGV.size != 1
-  puts "Usage: "
