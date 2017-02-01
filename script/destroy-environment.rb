6755011ceb3700ba4dd524ad5287741334ad4c71
Matteo Vaccari <mvaccari@thoughtworks.com>
2017-02-01 10:13:53 +0100

    fix race condition that could break instance permissions on the table

--- script/destroy-environment.rb
+++ script/destroy-environment.rb
@@ -12,8 +12,6 @@ include Infrastructure
 
 @env = ARGV[0]
 
-delete_roles @env
-delete_tables @env
 delete_instances @env
 find_all_instances(@env).each { |i|
   next if i.state.name == 'terminated'
@@ -21,4 +19,9 @@ find_all_instances(@env).each { |i|
   i.wait_until_terminated
 }
 delete_security_groups @env
+delete_roles @env
+
+# delete tables last, so that we avoid the risk of instances
+# keeping a surviving role that points to a deleted table
+delete_tables @env
 
