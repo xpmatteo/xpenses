dd0dca6acaf801376303336c63513e5f5e174ae4
Matteo Vaccari <mvaccari@thoughtworks.com>
2017-02-11 09:47:31 +0100

    saving two broken tests to remember where to continue

--- test/import_isp_integration_test.rb
+++ test/import_isp_integration_test.rb
@@ -9,23 +9,30 @@ require 'rack/test'
 
 class ImportIspIntegrationTest < Minitest::Test
 
+  TEST_FILE = 'test-data/isp-movements-short.xls'
+
   def setup
+    Account.new.clear
     @session = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
   end
 
-  def teardown
-  end
-
   def test_summary_row_empty
-    @session.get '/api/summary'
-    # table = session.find('#summary-table tbody')
-    # table.find('td:nth-of-type(1)').assert_text('2016-09')
-    # table.find('td:nth-of-type(2)').assert_text('750.53')
+    assert_equal "[]", @session.get('/api/summary').body
   end
 
-  def xtest_upload
-    test_file = 'test-data/isp-movements-short.xls'
-    @session.post "/api/movements", "file" => Rack::Test::UploadedFile.new(test_file, "application/xcel")
+  def test_summary_row_september
+    Account.new.load TEST_FILE
+    assert_equal '[{"month": "2016-09", "total": "750.53"}]', @session.get('/api/summary').body
+  end
+
+  def test_upload
+    @session.post "/api/movements", "file" => Rack::Test::UploadedFile.new(TEST_FILE, "application/xcel")
     assert @session.last_response.ok?, "last response #{@session.last_response.status}"
   end
+
+  def test_movements
+    @session.post "/api/movements", "file" => Rack::Test::UploadedFile.new(TEST_FILE, "application/xcel")
+    assert_equal '[...]', @session.get('/api/movements').body
+  end
+
 end
